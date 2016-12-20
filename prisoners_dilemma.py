447552a176b4db64cb3c5679c3c8f354dfcb779a
IsaacTebben21 <isaac.tebben@students.sacs.k12.in.us>
2016-12-20 08:25:08 -0500

    pull request
    pull request

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -36,7 +36,7 @@ import example0, example1, example2, example3
 import example4, example5, example6, example7
 import team0, team1, team2, Madeline Rotundo, team4
 import Lance Williams, team6, team7, team8, team9
-import team10, team11, team12, team13, team14
+import team10, team11, team12, team13, IsaacTebben
 betray = example1
 collude = example0
 
